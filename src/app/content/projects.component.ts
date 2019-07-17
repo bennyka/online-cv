51ed48f895651d31d04713a4d452b3130b7c1939
bkarla <bkarla@dgverlag.de>
2019-07-17 10:43:38 +0200

    background- and text-color

--- src/app/content/projects.component.ts
+++ src/app/content/projects.component.ts
@@ -12,4 +12,13 @@ export class ProjectsComponent implements OnInit {
   ngOnInit() {
   }
 
+  projects = [
+    // tslint:disable-next-line:max-line-length
+    {title: 'Webseite', subtitle: 'Startup Internetauftritt', image: 'http://benjamin-karla.de/assets/images/projects/project-evofintech.png', text: 'Zuletzt habe ich ein junges Startup Unternehmen bei der Erstellung ihrer Webseite unterstützt. Das Projekt wurde mit HTML5, JavaScript und JQuery umgesetzt.'},
+    // tslint:disable-next-line:max-line-length
+    {title: 'Digitale Inventarisierung aller Art von Gegenständen', subtitle: '', image: 'http://benjamin-karla.de/assets/images/projects/project-inventory.jpg', text: 'Inventory unterstützt Sie bei jeglicher Art der Inventarisierung. Von der privaten DVD Sammlung, bis hin zur professionellen Geschäftsinventur.'},
+    // tslint:disable-next-line:max-line-length
+    {title: 'Binäre Risiko Analyse', subtitle: '', image: 'http://benjamin-karla.de/assets/images/projects/project-bra.jpg', text: 'Dieses Projekt ist im Rahmen meiner Bachelorthesis entstanden und basiert auf den Grundlagen von Ben Sapiro. Bei der Binary Risk Analysis (BRA) handelt es sich grundlegend um eine einfache, schnelle und effektive Methode zur IT-Risikoeinschätzung. Meine Arbeit erweitert das Grundkonzept um weitere Fragenkataloge zu neuen Bedrohungsszenarien. Derzeit befindet sich die App in der entwicklung.'}
+  ];
+
 }
